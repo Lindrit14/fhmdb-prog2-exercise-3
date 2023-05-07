a84368bd98626c44ffca03a084e980d96ac99576
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-05-07 15:00:12 +0200

    create custom exceptions and add dependencies to the pom

--- src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/MovieApiException.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/MovieApiException.java
@@ -1,4 +1,19 @@
 package at.ac.fhcampuswien.fhmdb.exceptions;
 
-public class MovieApiException {
+public class MovieApiException extends Exception {
+
+
+    public MovieApiException(){
+    }
+    public MovieApiException(String message){
+        super(message);
+    }
+
+    public MovieApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public MovieApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
